15aee17be58ec635b6bd3fd259d8765994c2e6a8
Eity2021 <Eity2021akter@gmail.com>
2025-11-19 22:43:52 +0600

    added dasboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
 export default function Home() {
-  return <div>Dashboard</div>;
+  return (
+    <div className="flex items-center justify-center h-full bg-gray-100">
+      <h1 className="text-4xl md:text-6xl font-bold text-gray-800">
+        Dashboard
+      </h1>
+    </div>
+  );
 }
